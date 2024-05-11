4f0d7d79721362d50b23143414260cc8b442d968
madhavarora03 <eragon.cube@outlook.com>
2024-05-11 16:46:30 +0530

    something

--- mini-project/comments/index.js
+++ mini-project/comments/index.js
@@ -16,32 +16,28 @@ app.get("/posts/:id/comments", (req, res) => {
 
 app.post("/posts/:id/comments", async (req, res) => {
   const commentId = randomBytes(4).toString("hex");
-
   const { content } = req.body;
 
   const comments = commentsByPostId[req.params.id] || [];
 
   comments.push({ id: commentId, content });
 
-  try {
-    await axios.post("http://localhost:4005/events", {
-      type: "Comment Added",
-      data: {
-        id: commentId,
-        content,
-        postId: req.params.id,
-      },
-    });
-  } catch (error) {
-    console.error(error);
-  }
   commentsByPostId[req.params.id] = comments;
+
+  await axios.post("http://localhost:4005/events", {
+    type: "Comment Created",
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+    },
+  });
 
   res.status(201).send(comments);
 });
 
 app.post("/events", (req, res) => {
-  console.log("Received Event", req.body.type);
+  console.log("Event Received", req.body.type);
 
   res.send({});
 });
--- mini-project/event-bus/index.js
+++ mini-project/event-bus/index.js
@@ -6,11 +6,23 @@ app.use(express.json());
 
 app.post("/events", async (req, res) => {
   const event = req.body;
+  try {
+    await axios.post("http://localhost:4000/events", event);
+  } catch (error) {
+    throw new Error(error);
+  }
 
-  await axios.post("http://localhost:4000/events", event);
-  await axios.post("http://localhost:4001/events", event);
-  await axios.post("http://localhost:4002/events", event);
+  try {
+    await axios.post("http://localhost:4001/events", event);
+  } catch (error) {
+    throw new Error(error);
+  }
 
+  try {
+    await axios.post("http://localhost:4002/events", event);
+  } catch (error) {
+    throw new Error(error);
+  }
   res.send({ status: "OK" });
 });
 
