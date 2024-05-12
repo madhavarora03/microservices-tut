f7be3d9f54c33d486d6e91740b52ae85ef51acb0
madhavarora03 <eragon.cube@outlook.com>
2024-05-12 07:12:21 +0530

    Adding event syncing

--- mini-project/event-bus/index.js
+++ mini-project/event-bus/index.js
@@ -4,8 +4,13 @@ const app = express();
 
 app.use(express.json());
 
+const events = [];
+
 app.post("/events", async (req, res) => {
   const event = req.body;
+
+  events.push(event);
+
   try {
     await axios.post("http://localhost:4000/events", event);
   } catch (error) {
@@ -31,5 +36,7 @@ app.post("/events", async (req, res) => {
   }
   res.send({ status: "OK" });
 });
+
+app.get("/events", (req, res) => res.send(events));
 
 app.listen(4005, () => console.log("Listening on 4005"));
--- mini-project/query/index.js
+++ mini-project/query/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import axios from "axios";
 
 const app = express();
 
@@ -8,13 +9,7 @@ app.use(cors());
 
 const posts = {};
 
-app.get("/posts", (req, res) => {
-  res.send(posts);
-});
-
-app.post("/events", (req, res) => {
-  const { type, data } = req.body;
-
+const handleEvent = (type, data) => {
   if (type === "Post Created") {
     const { id, title } = data;
 
@@ -37,10 +32,29 @@ app.post("/events", (req, res) => {
     comment.status = status;
     comment.content = content;
   }
+};
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  handleEvent(type, data);
 
   console.log(posts);
 
   res.send({});
 });
 
-app.listen(4002, () => console.log("Listening on 4002"));
+app.listen(4002, async () => {
+  console.log("Listening on 4002");
+  const res = await axios.get("http://localhost:4005/events");
+
+  for (let event of res.data) {
+    console.log("Processing Event:", event.type);
+
+    handleEvent(event.type, event.data);
+  }
+});
