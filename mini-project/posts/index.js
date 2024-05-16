c039f9a8641142f8fd84c501ddd0689734c58445
madhavarora03 <eragon.cube@outlook.com>
2024-05-16 20:02:37 +0530

    Final done

--- mini-project/posts/index.js
+++ mini-project/posts/index.js
@@ -14,13 +14,14 @@ app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/posts", async (req, res) => {
+app.post("/posts/create", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
 
   posts[id] = { id, title };
+
   try {
-    await axios.post("http://localhost:4005/events", {
+    await axios.post("http://event-bus-srv:4005/events", {
       type: "Post Created",
       data: {
         id,
